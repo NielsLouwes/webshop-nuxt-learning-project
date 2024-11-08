e84e312adaeeabc043138a29a755f55b885a8a53
Niels Louwes <nielslouwes45@gmail.com>
2024-11-08 11:48:54 +0100

    add filtering products

--- pages/index.vue
+++ pages/index.vue
@@ -11,17 +11,44 @@ export type Product = {
   price: number;
   image: string;
   rating: Rating;
+  category: string;
 };
 
 const { data: products } = useFetch<Product[]>(
   "https://fakestoreapi.com/products"
 );
+
+const selectedCategory = ref("All");
+
+const categories = products.value?.map((product) => product.category);
+const uniqueCategories = [...new Set(categories)];
+
+console.log("uniqueCategories", uniqueCategories);
+
+const filteredProducts = computed(() => {
+  if (selectedCategory.value === "All") {
+    return products.value;
+  }
+  return products.value?.filter(
+    (product) => product.category === selectedCategory.value
+  );
+});
 </script>
 
 <template>
   <div class="container mx-auto">
+    <div class="flex gap-2 mt-4">
+      <h2 class="text-xl font-bold">Filters</h2>
+      <select class="border-2 p-1" v-model="selectedCategory">
+        <option>All</option>
+        <option v-for="category in uniqueCategories" :key="category">
+          {{ category }}
+        </option>
+      </select>
+    </div>
+
     <div class="flex flex-wrap gap-4 p-3 m-3">
-      <div v-for="product in products" :key="product.id">
+      <div v-for="product in filteredProducts" :key="product.id">
         <ProductCard :product="product" />
       </div>
     </div>
